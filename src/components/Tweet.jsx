0b13a3148e25bde2692cad95d8ff8564bdf2445f
Matan Post <matanpost@gmail.com>
2023-02-20 17:47:01 +0100

    done withon looping on the array with map()

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,34 +1,27 @@
-function Tweet() {
+import ProfileImage from './ProfileImage'
+import Action from './Actions'
+import User from './User'
+import Message from './Message'
+import Timestamp from './Timestamp'
+
+function Tweet({tweet}) {
+
   return (
     <div className="tweet">
-      <img
-        src="https://i.imgur.com/9yw1Fyw.jpg"
-        className="profile"
-        alt="profile"
-      />
+      
+      <ProfileImage image={tweet.user.image} />
 
       <div className="body">
         <div className="top">
-          <span className="user">
-            <span className="name">Ironhack</span>
-            <span className="handle">@ironhack</span>
-          </span>
+          <User name={tweet.user.name} handle={tweet.user.handle}  />
+          
 
-          <span className="timestamp">Nov 30, 2020</span>
+          <Timestamp timestamp={tweet.timestamp}/>
         </div>
 
-        <p className="message">
-          On December 7th, we will be hosting a #webinar that will introduce you
-          to #SQL! Are you ready? 🚀
-        </p>
+        <Message message={tweet.message}/>
 
-        <div className="actions">
-          {/* Font Awesome icons */}
-          <i className="far fa-comment"></i>
-          <i className="fas fa-retweet"></i>
-          <i className="far fa-heart"></i>
-          <i className="fas fa-share"></i>
-        </div>
+        <Action />
       </div>
 
       <i className="fas fa-ellipsis-h"></i>
